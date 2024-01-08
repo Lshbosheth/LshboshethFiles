87969617d315a444e6e302ad4c3fee402779cad9
lizezhi <1>
2024-01-08 17:05:09 +0800

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,28 +1,28 @@
 import request from '../axios'
 
 export const upload = (params: any) => {
-    return request.post('/upload', params)
+    return request.post('/files/uploadVercel', params)
 }
 
 export const getAllFile = (params: any) => {
-    return request.get('/fileManage', params)
+    return request.get('/files/vercelFiles', params)
 }
 
 export const deleteFile = (id: string) => {
-    return request.delete('/fileManage/' + id, {})
+    return request.delete('/files/vercelFiles/' + id, {})
 }
 
 export const getUploadQiniuToken = () => {
-    return request.get('/qiniu/uploadToken', {})}
+    return request.get('/files/qiniuFiles/uploadToken', {})}
 
 export const uploadQiniu = (data: any) => {
     return request.request('POST', 'https://upload-z1.qiniup.com', {data})
 }
 
 export const getQiniuData = () => {
-    return request.get('/qiniu', {})
+    return request.get('/files/qiniuFiles', {})
 }
 
 export const deleteQiniuFile = (key: string) => {
-    return request.delete('/qiniu/' + key, {})
+    return request.delete('/files/qiniuFiles/' + key, {})
 }
