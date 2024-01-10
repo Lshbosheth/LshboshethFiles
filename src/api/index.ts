98c65bdc32cdee63eac354a7ae9b54040cc74e67
lshbosheth <1>
2024-01-10 14:53:38 +0800

    init

---
+++ src/api/index.ts
@@ -0,0 +1,32 @@
+import request from '../axios'
+
+export const upload = (params: any) => {
+    return request.post('/files/uploadVercel', params)
+}
+
+export const getAllFile = (params: any) => {
+    return request.get('/files/vercelFiles', params)
+}
+
+export const deleteFile = (id: string) => {
+    return request.delete('/files/vercelFiles/' + id, {})
+}
+
+export const getUploadQiniuToken = () => {
+    return request.get('/files/qiniuFiles/uploadToken', {})}
+
+export const uploadQiniu = (data: any) => {
+    return request.request('POST', 'https://upload-z1.qiniup.com', {data})
+}
+
+export const getQiniuData = (params: any) => {
+    return request.get('/files/qiniuFiles', params)
+}
+
+export const deleteQiniuFile = (key: string) => {
+    return request.delete('/files/qiniuFiles/' + key, {})
+}
+
+export const deleteSomeQiniuFile = (params: any) => {
+    return request.post('/files/qiniuFiles/deleteSome', params)
+}
---
+++ src/axios/index.ts
@@ -0,0 +1,46 @@
+import type { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import axios from "axios";
+
+const service: AxiosInstance = axios.create({
+    baseURL: import.meta.env.PROD ? 'https://api.lshbosheth.cn/api' : 'http://localhost:1222/api',
+    timeout: 60 * 1000, // 请求超时时间
+    // headers: { "Content-Type": "application/json;charset=UTF-8" },
+    responseType: 'json'
+});
+
+service.interceptors.response.use((response: AxiosResponse) => {
+        const data = response.data;
+        return data;
+    },
+    (err) => {
+        return Promise.reject(err.response);
+    }
+);
+
+const request = {
+    get<T = any>(url: string, data?: any): Promise<T> {
+        return request.request("GET", url, { params: data });
+    },
+    post<T = any>(url: string, data?: any): Promise<T> {
+        return request.request("POST", url, { data });
+    },
+    put<T = any>(url: string, data?: any): Promise<T> {
+        return request.request("PUT", url, { data });
+    },
+    delete<T = any>(url: string, data?: any): Promise<T> {
+        return request.request("DELETE", url, { params: data });
+    },
+    request<T = any>(method = "GET", url: string, data?: any): Promise<T> {
+        return new Promise((resolve, reject) => {
+            service({ method, url, ...data })
+                .then((res) => {
+                    resolve(res as unknown as Promise<T>);
+                })
+                .catch((e: Error | AxiosError) => {
+                    reject(e);
+                })
+        });
+    }
+};
+
+export default request;
