1a6ca783161558f9d7c59e900e3c18b0b0129d37
lizezhi <1>
2024-02-04 11:27:40 +0800

    Build Version to 0.0.2

--- build.cjs
+++ build.cjs
@@ -5,15 +5,20 @@ const { execSync } = require('child_process');
 
 const commitMsg = process.argv.slice(2).join(' ');
 console.log(commitMsg);
-const configFile = './src-tauri/tauri.conf.json';
-const configContent = fs.readFileSync(configFile, 'utf-8');
-const config = JSON.parse(configContent);
+const tauriConfigFile = './src-tauri/tauri.conf.json';
+const packageFile = 'package.json';
+const tauriConfigContent = fs.readFileSync(tauriConfigFile, 'utf-8');
+const packageContent = fs.readFileSync(packageFile, 'utf-8');
+const tauriConfig = JSON.parse(tauriConfigContent);
+const packageConfig = JSON.parse(packageContent);
 
 // 增加 version 字段
 if(commitMsg.indexOf('build') > -1) {
-    config.package.version = commitMsg.split(' ')[commitMsg.split(' ').length - 1];
-    fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
+    tauriConfig.package.version = commitMsg.split(' ')[commitMsg.split(' ').length - 1];
+    packageConfig.version = commitMsg.split(' ')[commitMsg.split(' ').length - 1];
+    fs.writeFileSync(tauriConfigFile, JSON.stringify(tauriConfig, null, 2));
+    fs.writeFileSync(packageFile, JSON.stringify(packageConfig, null, 2));
     execSync('git add .', { stdio: 'inherit' });
-    execSync(`git commit -m "Build Version to ${config.package.version}"`, { stdio: 'inherit' });
+    execSync(`git commit -m "Build Version to ${tauriConfig.package.version}"`, { stdio: 'inherit' });
 }
 
