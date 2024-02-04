fc0cb65c4527b52693809bdf738c9271d71fa95b
lizezhi <1>
2024-02-04 16:11:39 +0800

    Build Version to 0.0.4

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,4 @@
 <script setup lang="ts">
-import {
-  checkUpdate,
-  installUpdate,
-  onUpdaterEvent,
-} from '@tauri-apps/api/updater'
-import { relaunch } from '@tauri-apps/api/process'
-const unlisten = await onUpdaterEvent(({ error, status }) => {
-  console.log('Updater event', error, status)
-})
-
-try {
-  const { shouldUpdate, manifest } = await checkUpdate()
-
-  if (shouldUpdate) {
-    console.log(
-        `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-    )
-
-    await installUpdate()
-    await relaunch()
-  }
-} catch (error) {
-  console.error(error)
-}
-
-unlisten()
 </script>
 
 <template>
