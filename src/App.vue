f0d447dfcef921cec5a0ac3df7cb3961a53ab22d
lizezhi <1>
2024-02-04 16:37:38 +0800

    Build Version to 0.0.5

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,4 @@
 </template>
 
 <style scoped>
-@import "./assets/iconfont/iconfont.css";
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
