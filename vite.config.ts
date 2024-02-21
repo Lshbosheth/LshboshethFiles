2f65e03d95c5eaa61b1f0471ad80c55946e2b383
lizezhi <1>
2024-02-21 13:22:08 +0800

    test pwa 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -19,6 +20,27 @@ export default defineConfig({
       Components({
         resolvers: [ElementPlusResolver()],
       }),
+      VitePWA({
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon-512x512.png'],
+          manifest: {
+              name: 'My Awesome App',
+              short_name: 'MyApp',
+              description: 'My Awesome App description',
+              theme_color: '#ffffff',
+              icons: [
+                  {
+                      src: 'pwa-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png'
+                  }
+              ]
+          }
+      })
   ],
   server: {
     port: 1030
